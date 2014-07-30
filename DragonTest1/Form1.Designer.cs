93c4b9684802504846eeb896588ce56b8f87403c
johnofaurora <johnofaurora@gmail.com>
2014-07-30 12:27:23 -0500

    initial commit

---
+++ DragonTest1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DragonTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToCernerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadTemplateToolStripMenuItem,
+            this.copyToCernerToolStripMenuItem,
+            this.nextToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadTemplateToolStripMenuItem
+            // 
+            this.loadTemplateToolStripMenuItem.Name = "loadTemplateToolStripMenuItem";
+            this.loadTemplateToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.loadTemplateToolStripMenuItem.Text = "Load Template";
+            this.loadTemplateToolStripMenuItem.Click += new System.EventHandler(this.loadTemplateToolStripMenuItem_Click);
+            // 
+            // copyToCernerToolStripMenuItem
+            // 
+            this.copyToCernerToolStripMenuItem.Name = "copyToCernerToolStripMenuItem";
+            this.copyToCernerToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.copyToCernerToolStripMenuItem.Text = "Copy To Cerner";
+            this.copyToCernerToolStripMenuItem.Click += new System.EventHandler(this.copyToCernerToolStripMenuItem_Click);
+            // 
+            // nextToolStripMenuItem
+            // 
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.nextToolStripMenuItem.Text = "Next";
+            this.nextToolStripMenuItem.Click += new System.EventHandler(this.nextToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(934, 701);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 725);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadTemplateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToCernerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ DragonTest1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DragonTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        //comment
+
+        string placeholder = "***";
+
+        private void loadTemplateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        String line = sr.ReadToEnd();
+                        textBox1.Text = line;
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("File could not be read");
+                    return;
+                }
+                nextToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+        private void nextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int cursPos = textBox1.SelectionStart;
+            int nextIndex = textBox1.Text.IndexOf(placeholder, cursPos); //if curently at a ***, won't move past
+            if (nextIndex < 0) 
+            {
+                cursPos = 0;
+                nextIndex = textBox1.Text.IndexOf(placeholder, cursPos);
+                if (nextIndex < 0)
+                {
+                    nextIndex = 0;
+                    MessageBox.Show("End of template reached. \rNo \"" + placeholder +"\" detected.");
+                }
+            }
+            //MessageBox.Show(cursPos.ToString() + "   " + nextIndex.ToString()); 
+            textBox1.SelectionStart = nextIndex;
+            textBox1.SelectionLength = placeholder.Length;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyData.ToString());
+            if (e.KeyData.ToString() == "F1")
+            {
+                loadTemplateToolStripMenuItem_Click(sender, e);
+            }
+            if (e.KeyData.ToString() == "F2")
+            {
+                nextToolStripMenuItem_Click(sender, e);
+            }
+            if (e.KeyData.ToString() == "F3")
+            {
+                copyToCernerToolStripMenuItem_Click(sender, e);
+            }
+
+        }
+
+        private void copyToCernerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var prc = Process.GetProcessesByName("hostex32");
+
+            if (prc.Length > 0)
+            {
+                //MessageBox.Show("found something");
+                SetForegroundWindow(prc[0].MainWindowHandle);
+                SendKeys.Send(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Could not find Cerner");
+            }
+        }
+
+
+    }
+}
